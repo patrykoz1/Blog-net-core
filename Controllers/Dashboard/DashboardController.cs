34c1f823e83358ada373b6e54f7d957992c07f05
patryk_zur <patryk.zur@initsoft.pl>
2020-10-05 13:48:09 +0200

    Editing Article

---
+++ Controllers/Dashboard/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private IArticleRepository _articlesRepository;
+
+        public DashboardController(IArticleRepository articleRepository)
+        {
+            _articlesRepository = articleRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteArticle()
+        {
+            return View(_articlesRepository.GetArticles());
+        }
+
+        public IActionResult EditArticle()
+        {
+            return View(_articlesRepository.GetArticles());
+        }
+
+        public IActionResult AddArticle()
+        {
+            return View();
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Dashboard/EditPartialViewController.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Blog.Controllers.Dashboard
+{
+    public class EditPartialViewController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public EditPartialViewController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public IActionResult TableSeeder(int id)
+        {
+            Article model = context.Articles.Where(x => x.Id == id).ToList()[0];// only one element
+            String xd = id.ToString();
+            
+            return PartialView("~/Views/Dashboard/EditPartialView.cshtml", model);
+        }
+    }
+}
